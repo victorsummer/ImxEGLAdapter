8ef85b27c2257b3243a31e0e75153e6277cb363b
Daniel Ranger <dranger003@gmail.com>
2014-01-30 19:17:08 +0000

    Preliminary support for IMX6Q.

--- main.c
+++ main.c
@@ -26,6 +26,8 @@ struct egl_device {
     const EGLint *attr_context;
 };
 
+volatile sigatomic_t done = 0;
+
 void clock_gettime_diff(
     const struct timespec *time_beg,
     const struct timespec *time_end,
@@ -108,231 +110,289 @@ struct timespec timespec_add_safe(const struct timespec lhs,
 }
 
 /***************************************************************************************************
-  X11 platform
+  Template platform
 
 ***************************************************************************************************/
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
 
-#include <pthread.h>
+//int egl_platform_get_display_type(struct egl_device *device)
+//{
+//    return 0;
+//}
 
-#define EGL_DEVICE_NAME     "Open GL ES 2"
-#define EGL_DEVICE_X        0
-#define EGL_DEVICE_Y        0
-#define EGL_DEVICE_WIDTH    1280
-#define EGL_DEVICE_HEIGHT   576
+//int egl_platform_create_window(struct egl_device *device)
+//{
+//    return 0;
+//}
+
+//int egl_platform_destroy_window(struct egl_device *device)
+//{
+//    return 0;
+//}
+
+//void egl_platform_run(struct egl_device *device)
+//{
+//}
+
+/**************************************************************************************************/
+
+/***************************************************************************************************
+  IMX6Q FB platform
+
+***************************************************************************************************/
 
 int egl_platform_get_display_type(struct egl_device *device)
 {
-    XInitThreads();
+    device->type = (EGLNativeDisplayType)fbGetDisplayByIndex(0);
+    fbGetDisplayGeometry(device->display_type, &device->width, &device->height);
 
-    device->type = (EGLNativeDisplayType)XOpenDisplay(0);
     return 0;
 }
 
 int egl_platform_create_window(struct egl_device *device)
 {
-    static EGLint attr_config[] = {
-        EGL_RED_SIZE,           1,
-        EGL_GREEN_SIZE,         1,
-        EGL_BLUE_SIZE,          1,
-        EGL_DEPTH_SIZE,         1,
-        EGL_RENDERABLE_TYPE,    EGL_OPENGL_ES2_BIT,
-        EGL_NONE
-    };
-
-    Window root = RootWindow(device->type, DefaultScreen(device->type));
-
-    EGLConfig config;
-    EGLint config_count;
-    eglChooseConfig(device->display, attr_config, &config, 1, &config_count);
-    assert(config);
-    assert(config_count > 0);
-
-    EGLint visual_id;
-    eglGetConfigAttrib(device->display, config, EGL_NATIVE_VISUAL_ID, &visual_id);
-
-    XVisualInfo visual_templ;
-    visual_templ.visualid = visual_id;
-
-    int visual_count;
-    XVisualInfo *visual_info = XGetVisualInfo(device->type, VisualIDMask, &visual_templ, &visual_count);
-
-    XSetWindowAttributes attr = { 0 };
-    attr.colormap = XCreateColormap(device->type, root, visual_info->visual, AllocNone);
-    attr.event_mask = StructureNotifyMask | ExposureMask | KeyPressMask;
-
-    device->window = (EGLNativeWindowType)XCreateWindow(
-        device->type,
-        root,
-        0,
-        0,
-        EGL_DEVICE_WIDTH,
-        EGL_DEVICE_HEIGHT,
-        0,
-        visual_info->depth,
-        InputOutput,
-        visual_info->visual,
-        CWBackPixel | CWBorderPixel | CWColormap | CWEventMask,
-        &attr);
-
-    XFree(visual_info);
-
-    XSizeHints size_hints;
-    size_hints.x = EGL_DEVICE_X;
-    size_hints.y = EGL_DEVICE_Y;
-    size_hints.width  = EGL_DEVICE_WIDTH;
-    size_hints.height = EGL_DEVICE_HEIGHT;
-    size_hints.flags = USSize | USPosition;
-
-    XSetNormalHints(device->type, device->window, &size_hints);
-
-    XSetStandardProperties(
-        device->type,
-        device->window,
-        EGL_DEVICE_NAME,
-        EGL_DEVICE_NAME,
-        None,
-        (char **)NULL,
-        0,
-        &size_hints);
-
-    XMapWindow(device->type, device->window);
+    device->window = (EGLNativeWindowType)fbCreateWindow(device->display_type, 0, 0, 0, 0);
 
     return 0;
 }
 
 int egl_platform_destroy_window(struct egl_device *device)
 {
-    XDestroyWindow(device->type, device->window);
-    XCloseDisplay(device->type);
-
-    return 0;
-}
-
-volatile int done = 0;
-
-static void *thread_start(void *arg)
-{
-    struct egl_device *device = (struct egl_device *)arg;
-
-    int x11_fd = ConnectionNumber(device->type);
-    fd_set in_fds;
-    struct timeval timer;
-
-    while (!done) {
-        FD_ZERO(&in_fds);
-        FD_SET(x11_fd, &in_fds);
-
-        timer.tv_usec = 0;
-        timer.tv_sec = 1;
-
-        select(x11_fd + 1, &in_fds, 0, 0, &timer);
-
-        while (XPending(device->type)) {
-            XEvent event;
-            XNextEvent(device->type, &event);
-            switch (event.type)
-            {
-                case Expose:
-                    // Redraw
-                    break;
-                case ConfigureNotify:
-                    glViewport(0, 0, (GLint)event.xconfigure.width, (GLint)event.xconfigure.height);
-                    break;
-                case KeyPress:
-                    {
-                        XLookupKeysym(&event.xkey, 0);
-                        char buf[10];
-                        XLookupString(&event.xkey, buf, sizeof(buf), 0, 0);
-                        if (buf[0] == 27)
-                            done = 1;
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-
-    }
+    fbDestroyWindow(device->window);
+    device->window = (EGLNativeWindowType)0;
 
     return 0;
 }
 
 void egl_platform_run(struct egl_device *device)
 {
-    system("setterm -cursor off");
-
-    pthread_t thread;
-    pthread_create(&thread, 0, &thread_start, (void *)device);
-
-    int d = 0;
-    float i = 0.0;
-    int frame_count = 0;
-
-    struct timespec time_slice = { 0 }, time_start, time_target, time_sync, time_loop_b, time_loop_e = { 0 };
-    long elapsed = 0;
-
-    time_slice.tv_nsec = 1 / (24000 / 1001.0L) * 1000000000L;
-    clock_gettime(CLOCK_MONOTONIC, &time_start);
-    time_sync = time_target = time_start;
-
-    while (!done) {
-        time_loop_b = time_loop_e;
-        clock_gettime(CLOCK_MONOTONIC, &time_loop_e);
-        time_target = time_loop_e;
-
-        {
-            glClearColor(i, i, i, 1);
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            eglSwapBuffers(device->display, device->surface);
-
-            if (d)
-                i += 0.01;
-            else
-                i -= 0.01;
-
-            if (i > 1.0 || i < 0.0) {
-                if (i > 1.0)
-                    i = 1.0;
-                if (i < 0.0)
-                    i = 0.0;
-
-                d = !d;
-            }
-        }
-
-        ++frame_count;
-
-        fprintf(
-            stdout,
-            "F: %d, E: %.3Lf, C: %.3Lf       \r",
-            frame_count,
-            elapsed / 1000000000.0L,
-            1 / (time_loop_e.tv_sec - time_loop_b.tv_sec + (time_loop_e.tv_nsec - time_loop_b.tv_nsec) / 1000000000.0L)
-        );
-        fflush(stdout);
-
-        time_target = timespec_add_safe(time_target, time_slice);
-        while (time_sync.tv_sec * 1000000000 + time_sync.tv_nsec < time_target.tv_sec * 1000000000 + time_target.tv_nsec) {
-            clock_gettime(CLOCK_MONOTONIC, &time_sync);
-            elapsed = (time_sync.tv_sec - time_start.tv_sec) * 1000000000.0L + (time_sync.tv_nsec - time_start.tv_nsec);
-        }
-    }
-
-//    clock_gettime(CLOCK_MONOTONIC, &time_target);
-//    printf(
-//        "%.3Lf\n",
-//        frame_count / (time_target.tv_sec - time_start.tv_sec) + (time_target.tv_nsec - time_start.tv_nsec) / 1000000000.0L
-//    );
-
-    printf("\n");
-
-    pthread_join(thread, 0);
-
-    system("setterm -cursor on");
 }
+
+/**************************************************************************************************/
+
+/***************************************************************************************************
+  X11 platform
+
+***************************************************************************************************/
+//#include <X11/Xlib.h>
+//#include <X11/Xutil.h>
+
+//#include <pthread.h>
+
+//#define EGL_DEVICE_NAME     "Open GL ES 2"
+//#define EGL_DEVICE_X        0
+//#define EGL_DEVICE_Y        0
+//#define EGL_DEVICE_WIDTH    1280
+//#define EGL_DEVICE_HEIGHT   576
+
+//int egl_platform_get_display_type(struct egl_device *device)
+//{
+//    XInitThreads();
+
+//    device->type = (EGLNativeDisplayType)XOpenDisplay(0);
+//    return 0;
+//}
+
+//int egl_platform_create_window(struct egl_device *device)
+//{
+//    static EGLint attr_config[] = {
+//        EGL_RED_SIZE,           1,
+//        EGL_GREEN_SIZE,         1,
+//        EGL_BLUE_SIZE,          1,
+//        EGL_DEPTH_SIZE,         1,
+//        EGL_RENDERABLE_TYPE,    EGL_OPENGL_ES2_BIT,
+//        EGL_NONE
+//    };
+
+//    Window root = RootWindow(device->type, DefaultScreen(device->type));
+
+//    EGLConfig config;
+//    EGLint config_count;
+//    eglChooseConfig(device->display, attr_config, &config, 1, &config_count);
+//    assert(config);
+//    assert(config_count > 0);
+
+//    EGLint visual_id;
+//    eglGetConfigAttrib(device->display, config, EGL_NATIVE_VISUAL_ID, &visual_id);
+
+//    XVisualInfo visual_templ;
+//    visual_templ.visualid = visual_id;
+
+//    int visual_count;
+//    XVisualInfo *visual_info = XGetVisualInfo(device->type, VisualIDMask, &visual_templ, &visual_count);
+
+//    XSetWindowAttributes attr = { 0 };
+//    attr.colormap = XCreateColormap(device->type, root, visual_info->visual, AllocNone);
+//    attr.event_mask = StructureNotifyMask | ExposureMask | KeyPressMask;
+
+//    device->window = (EGLNativeWindowType)XCreateWindow(
+//        device->type,
+//        root,
+//        0,
+//        0,
+//        EGL_DEVICE_WIDTH,
+//        EGL_DEVICE_HEIGHT,
+//        0,
+//        visual_info->depth,
+//        InputOutput,
+//        visual_info->visual,
+//        CWBackPixel | CWBorderPixel | CWColormap | CWEventMask,
+//        &attr);
+
+//    XFree(visual_info);
+
+//    XSizeHints size_hints;
+//    size_hints.x = EGL_DEVICE_X;
+//    size_hints.y = EGL_DEVICE_Y;
+//    size_hints.width  = EGL_DEVICE_WIDTH;
+//    size_hints.height = EGL_DEVICE_HEIGHT;
+//    size_hints.flags = USSize | USPosition;
+
+//    XSetNormalHints(device->type, device->window, &size_hints);
+
+//    XSetStandardProperties(
+//        device->type,
+//        device->window,
+//        EGL_DEVICE_NAME,
+//        EGL_DEVICE_NAME,
+//        None,
+//        (char **)NULL,
+//        0,
+//        &size_hints);
+
+//    XMapWindow(device->type, device->window);
+
+//    return 0;
+//}
+
+//int egl_platform_destroy_window(struct egl_device *device)
+//{
+//    XDestroyWindow(device->type, device->window);
+//    XCloseDisplay(device->type);
+
+//    return 0;
+//}
+
+//static void *thread_start(void *arg)
+//{
+//    struct egl_device *device = (struct egl_device *)arg;
+
+//    int x11_fd = ConnectionNumber(device->type);
+//    fd_set in_fds;
+//    struct timeval timer;
+
+//    while (!done) {
+//        FD_ZERO(&in_fds);
+//        FD_SET(x11_fd, &in_fds);
+
+//        timer.tv_usec = 0;
+//        timer.tv_sec = 1;
+
+//        select(x11_fd + 1, &in_fds, 0, 0, &timer);
+
+//        while (XPending(device->type)) {
+//            XEvent event;
+//            XNextEvent(device->type, &event);
+//            switch (event.type)
+//            {
+//                case Expose:
+//                    // Redraw
+//                    break;
+//                case ConfigureNotify:
+//                    glViewport(0, 0, (GLint)event.xconfigure.width, (GLint)event.xconfigure.height);
+//                    break;
+//                case KeyPress:
+//                    {
+//                        XLookupKeysym(&event.xkey, 0);
+//                        char buf[10];
+//                        XLookupString(&event.xkey, buf, sizeof(buf), 0, 0);
+//                        if (buf[0] == 27)
+//                            done = 1;
+//                    }
+//                    break;
+//                default:
+//                    break;
+//            }
+//        }
+
+//    }
+
+//    return 0;
+//}
+
+//void egl_platform_run(struct egl_device *device)
+//{
+//    system("setterm -cursor off");
+
+//    pthread_t thread;
+//    pthread_create(&thread, 0, &thread_start, (void *)device);
+
+//    int d = 0;
+//    float i = 0.0;
+//    int frame_count = 0;
+
+//    struct timespec time_slice = { 0 }, time_start, time_target, time_sync, time_loop_b, time_loop_e = { 0 };
+//    long elapsed = 0;
+
+//    time_slice.tv_nsec = 1 / (24000 / 1001.0L) * 1000000000L;
+//    clock_gettime(CLOCK_MONOTONIC, &time_start);
+//    time_sync = time_target = time_start;
+
+//    while (!done) {
+//        time_loop_b = time_loop_e;
+//        clock_gettime(CLOCK_MONOTONIC, &time_loop_e);
+//        time_target = time_loop_e;
+
+//        {
+//            glClearColor(i, i, i, 1);
+//            glClear(GL_COLOR_BUFFER_BIT);
+
+//            eglSwapBuffers(device->display, device->surface);
+
+//            if (d)
+//                i += 0.01;
+//            else
+//                i -= 0.01;
+
+//            if (i > 1.0 || i < 0.0) {
+//                if (i > 1.0)
+//                    i = 1.0;
+//                if (i < 0.0)
+//                    i = 0.0;
+
+//                d = !d;
+//            }
+//        }
+
+//        ++frame_count;
+
+//        fprintf(
+//            stdout,
+//            "F: %d, E: %.3Lf, C: %.3Lf       \r",
+//            frame_count,
+//            elapsed / 1000000000.0L,
+//            1 / (time_loop_e.tv_sec - time_loop_b.tv_sec + (time_loop_e.tv_nsec - time_loop_b.tv_nsec) / 1000000000.0L)
+//        );
+//        fflush(stdout);
+
+//        time_target = timespec_add_safe(time_target, time_slice);
+//        while (time_sync.tv_sec * 1000000000 + time_sync.tv_nsec < time_target.tv_sec * 1000000000 + time_target.tv_nsec) {
+//            clock_gettime(CLOCK_MONOTONIC, &time_sync);
+//            elapsed = (time_sync.tv_sec - time_start.tv_sec) * 1000000000.0L + (time_sync.tv_nsec - time_start.tv_nsec);
+//        }
+//    }
+
+////    clock_gettime(CLOCK_MONOTONIC, &time_target);
+////    printf(
+////        "%.3Lf\n",
+////        frame_count / (time_target.tv_sec - time_start.tv_sec) + (time_target.tv_nsec - time_start.tv_nsec) / 1000000000.0L
+////    );
+
+//    printf("\n");
+
+//    pthread_join(thread, 0);
+
+//    system("setterm -cursor on");
+//}
 
 /**************************************************************************************************/
 
